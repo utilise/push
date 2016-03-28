8d3ef5eb7103a9a08890eb8b41f645429036cd2b
pemrouz <p3pedram@hotmail.com>
2016-03-28 01:27:33 +0100

    update to latest set

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
-var versioned = require('versioned').default
-  , expect = require('chai').expect
+var expect = require('chai').expect
   , last = require('utilise.last')
+  , set = require('utilise.set')
   , push = require('./')
 
 describe('push', function() {
@@ -10,25 +10,16 @@ describe('push', function() {
   })
 
   it('should push - versioned', function(){
-    var changes = []
-      , o = versioned([]).on('log', function(diff){ changes.push(diff) })
+    var o = set()([])
 
-    expect(o).to.eql([])
-    expect(o.log.length).to.eql(1) 
-    expect(last(o.log).diff).to.eql(undefined)
-    expect(last(o.log).value.toJS()).to.eql([])
-    expect(changes).to.eql([])
-
-    expect(push('foo')(o)).to.eql(o)
+    expect(push('foo')(o)).to.equal(o)
     expect(o).to.eql(['foo'])
     expect(o.log.length).to.eql(2)
-    expect(last(o.log).diff).to.eql({ key: '0', value: 'foo', type: 'add' })
-    expect(last(o.log).value.toJS()).to.eql(['foo'])
-    expect(changes).to.eql(o.log.slice(1).map(d => d.diff))
+    expect(last(o.log)).to.eql({ key: '0', value: 'foo', type: 'add', time: 1 })
   })
 
   it('should skip gracefully', function(){
-    expect(push('foo')(versioned({}))).to.be.eql({})
+    expect(push('foo')(set()({}))).to.be.eql({})
     expect(push('foo')({})).to.be.eql({})
     expect(push('foo')(true)).to.be.eql(true)
     expect(push('foo')(5)).to.be.eql(5)
